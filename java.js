8bb79f46bacf2fa17d5e7d36a22d5f27c07a693c
Douglas.Mariano <douglas.mariano@estudante.ufjf.br>
2023-06-20 20:05:44 -0300

    processos

--- java.js
+++ java.js
@@ -1,3 +1,28 @@
-* {
-    
+const casa = document.querySelectorAll(".casa");
+const disco = document.querySelector(".disco");
+
+disco.addEventListener('dragstart', arrastar);
+
+for (let i = 0; i<casa.length; i++) {
+    const casas = casa[i];
+    casa[1].addEventListener('dragover', passa);
+    casa[1].addEventListener('drop', recebe);
+}
+
+
+let arrastado = null;
+
+function arrastar(evento){
+
+}
+
+function recebe(evento){
+    if(arrastado){
+        evento.target.appendChild(arrastado);
+        arrastado = null;
+    }
+}
+
+function passa(evento){
+    evento.preventDefault();
 }
